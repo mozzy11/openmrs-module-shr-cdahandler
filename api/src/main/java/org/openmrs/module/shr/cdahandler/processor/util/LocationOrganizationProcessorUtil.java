b448763fa16f2d8d163643c4f2fefe430a071876
Arkadiusz Lalo <alalo@soldevelo.com>
2018-01-18 18:16:11 +0100

    #432: Fixed issue with the StringIndexOutOfBoundsException

--- api/src/main/java/org/openmrs/module/shr/cdahandler/processor/util/LocationOrganizationProcessorUtil.java
+++ api/src/main/java/org/openmrs/module/shr/cdahandler/processor/util/LocationOrganizationProcessorUtil.java
@@ -121,11 +121,19 @@ public final class LocationOrganizationProcessorUtil {
 	 */
 	public Location getOrganizationByExternalId(String id) throws DocumentImportException
 	{
+		try {
+			Integer locationId = Integer.valueOf(id.substring(0, id.indexOf("^")));
+			Location locationById = Context.getLocationService().getLocation(locationId);
+			if (locationById != null)
+				return locationById;
+		} catch (StringIndexOutOfBoundsException ex) {
+            throw new DocumentImportException("No data specified for location id");
+		}
 
-		Integer locationId = Integer.valueOf(id.substring(0,id.indexOf("^")));
-		Location locationById = Context.getLocationService().getLocation(locationId);
-		if(locationById!=null)
-			return locationById;
+		Location locationByUUid = Context.getLocationService().getLocationByUuid(id);
+		if (locationByUUid != null) {
+			return locationByUUid;
+		}
 
 		// TODO: This is an organization not a location so we need to get all locations belonging to the organization
 		// TODO: For now this is stored just as a location with an externalId tag
